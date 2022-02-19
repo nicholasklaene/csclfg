5e1ddc3745854165ba82e4a5c464c099b04480cc
nicholasklaene <nklaenedev@gmail.com>
2022-02-19 12:12:32 -0500

    stuck on circular dep, done for the day

--- backend/cognito/src/post_confirmation.py
+++ backend/cognito/src/post_confirmation.py
@@ -5,6 +5,7 @@ import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+cognito = boto3.client('cognito-idp')
 dynamodb = boto3.resource('dynamodb').Table(os.environ.get("TABLE"))
 
 def lambda_handler(event, context):
@@ -33,6 +34,12 @@ def lambda_handler(event, context):
     except Exception as exception:
         logger.error('Error inserting user: ' + str(exception))
 
+    try:
+        logger.info("Adding user to Users group")
+        username = event["request"]["userAttributes"]["username"]
+        cognito.admin_add_user_to_group(UserPoolId=os.environ.get("USER_POOL"), Username=username, GroupName="Users")
+    except Exception as exception:
+        logger.error("Error adding user to users group: " + str(exception))
 
     logger.info("Returning event: " + json.dumps(event))
     
