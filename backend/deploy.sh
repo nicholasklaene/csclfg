72a6a0f82a0ac8e60d43f76e88aa48484eaf5fe8
nicholasklaene <nklaenedev@gmail.com>
2022-02-22 17:33:29 -0500

    add test and prod env

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,6 +1,26 @@
+# TODO: setup pipeline
+read -p "Environment: " ENV
+
+if [ "$ENV" != "test" ] && [ "$ENV" != "prod" ];
+then
+    printf "Invalid environment. Aborting.\n"
+    exit 1
+fi
+
+if [ "$ENV" == "prod" ];
+then
+    read -p "Are you sure you want to deploy to production? (y/n) " CONFIRM_PROD_DEPLOY
+    
+    if [ "$CONFIRM_PROD_DEPLOY" != "y" ];
+    then
+        printf "Aborting"
+        exit 1
+    fi
+fi
+
 cd api
 bash build_layer.sh
 cd ..
 
 sam build
-echo "y" | sam deploy --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM
+echo "y" | sam deploy --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --config-env $ENV
