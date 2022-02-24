07ff46050d7fe67ed254ca2d16fcf9324b5d2e88
nicholasklaene <nklaenedev@gmail.com>
2022-02-24 16:52:40 -0500

    update nav

--- ui/src/components/AppNavbar.vue
+++ ui/src/components/AppNavbar.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import { useAuthStore } from "../stores/authStore";
+import { useRoute } from "vue-router";
+const currentRoute = useRoute();
 const authStore = useAuthStore();
 </script>
 
 <template>
   <nav class="max-w-screen-lg mx-auto p-4">
     <ul class="flex text-white">
+      <li>
+        <router-link
+          class="hover:underline"
+          to="/"
+          v-if="currentRoute.name !== 'Home'"
+        >
+          Home
+        </router-link>
+      </li>
       <li class="ml-auto">
         <template v-if="!authStore._isAuthenticated">
           <button
