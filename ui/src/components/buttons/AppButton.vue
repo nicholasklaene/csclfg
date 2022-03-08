7fa201e93ba26882bad41e11b163da916f747c90
nicholasklaene <nklaenedev@gmail.com>
2022-03-07 22:00:02 -0500

    disabled buttons

--- ui/src/components/buttons/AppButton.vue
+++ ui/src/components/buttons/AppButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="border border-blue-300 py-2 px-8 bg-blue-300 hover:opacity-90 p-2 text-gray-900"
+    class="border border-blue-300 py-2 px-8 bg-blue-300 hover:opacity-90 p-2 text-gray-900 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:border-gray-600 disabled:text-gray-400 disabled:hover:opacity-100"
   >
     <slot></slot>
   </button>
--- ui/src/views/NotFound.vue
+++ ui/src/views/NotFound.vue
@@ -4,6 +4,9 @@
       <h1 class="text-4xl text-white">
         Uh oh! The page you requested does not exist.
       </h1>
+      <p class="text-white text-center underline mt-4">
+        <router-link to="/">Home</router-link>
+      </p>
     </div>
   </div>
 </template>
