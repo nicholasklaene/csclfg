8582b45824654ef196d803579f597e5e5f38f5e0
nicholasklaene <nklaenedev@gmail.com>
2022-03-10 00:56:12 -0500

    sign in modal

--- web/src/components/AppSigninModal.vue
+++ web/src/components/AppSigninModal.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import AppModalBase from "./AppModalBase.vue";
+import AppSigninButton from "./AppSigninButton.vue";
+import AppSignupButton from "./AppSignupButton.vue";
 import { defineEmits } from "vue";
+
 const emit = defineEmits(["close"]);
 
 function close() {
@@ -11,7 +14,13 @@ function close() {
 <template>
   <AppModalBase @close="close()">
     <template v-slot:content>
-      <div>content!</div>
+      <div class="px-2">
+        <h4 class="text-center">You'll need to sign in to post</h4>
+        <div class="row gy-2">
+          <AppSigninButton />
+          <AppSignupButton />
+        </div>
+      </div>
     </template>
   </AppModalBase>
 </template>
