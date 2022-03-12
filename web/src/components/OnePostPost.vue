ca408e8dacae07ed4cf799513e20c757cfbe65a8
nicholasklaene <nklaenedev@gmail.com>
2022-03-12 12:10:42 -0500

    add link to user on post

--- web/src/components/OnePostPost.vue
+++ web/src/components/OnePostPost.vue
@@ -11,7 +11,18 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <div class="bg-backgroundCompliment p-4 mt-auto">
-    <div v-html="marked.parse(props.post.description)"></div>
+  <div class="bg-backgroundCompliment mt-auto">
+    <div class="mb-2 border-2 border-bottom border-background pt-4 px-4 pb-2">
+      <h5>
+        <span>User: </span>
+        <router-link class="text-text" :to="`/users/${props.post.user_id}`">
+          {{ props.post.user_id }}
+        </router-link>
+      </h5>
+    </div>
+    <div
+      class="pt-2 px-4 pb-4"
+      v-html="marked.parse(props.post.description)"
+    ></div>
   </div>
 </template>
