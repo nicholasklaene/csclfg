6df91cc002070987405d4e5db297bc15246bd452
nicholasklaene <nklaenedev@gmail.com>
2022-03-12 11:50:10 -0500

    remove redirection to logs for now

--- infrastructure/individual/deployOneSite.sh
+++ infrastructure/individual/deployOneSite.sh
@@ -2,5 +2,9 @@ read -p "App subdomain: " subdomain
 
 date=$(date '+%Y-%m-%d %H:%M:%S')
 
-sam build &> "./logs/$date-$subdomain-build.log" &
-sam deploy --config-file "./config/sam/$subdomain-samconfig.toml" &> "./logs/$date-$subdomain-deploy.log" &
+# For some reason log file redirection breaks the deployment
+# sam build &> "./logs/$date-$subdomain-build.log" &
+# sam deploy --config-file "./config/sam/$subdomain-samconfig.toml" &> "./logs/$date-$subdomain-deploy.log" &
+
+sam build
+sam deploy --config-file "./config/sam/$subdomain-samconfig.toml" 
