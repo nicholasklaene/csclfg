593dd156388c157318b0e1e788aa77889812bf6b
nicholasklaene <nklaenedev@gmail.com>
2022-03-26 02:39:04 -0400

    init angular workspace & apps

--- v2/ui/admin.studyseeking.com/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-// @ts-ignore
-import vuetify from "@vuetify/vite-plugin";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })]
-})
